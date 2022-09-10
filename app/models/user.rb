2da40638763497b8816784e0305ee1bae3056de8
goh <99309242+goh0222@users.noreply.github.com>
2022-09-10 11:00:14 +0800

    Add unit test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+
+  def full_name
+    "#{first_name.capitalize} #{last_name.capitalize}"
+  end
 end
